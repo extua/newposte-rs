d67c4ffda1bd2c1c569bfcedcb4e96d0b364cc9b
extua <pierre.marshall@gmail.com>
2023-11-10 18:11:52 +0800

    rename cargo.toml and run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,50 +4,51 @@ use std::io::stdin;
 use std::io::stdout;
 use std::io::{BufWriter, Write};
 
-
 fn main() {
-
     fn set_tags() -> String {
-        let mut tag_list:Vec<String> = Vec::new();
+        let mut tag_list: Vec<String> = Vec::new();
         println!("enter tag:");
         let mut inputted_string = set_input().trim().to_string();
         tag_list.push(inputted_string);
         println!("the tag list is {:?}", tag_list);
         println!("do you want to enter more tags? (y/n)");
         while set_input().to_lowercase().trim() == "y" {
-                println!("enter tag:");
-                let mut inputted_string = set_input().trim().to_string();
-                tag_list.push(inputted_string);
-                println!("the tag list is now {:?}", tag_list);
-                println!("do you want to enter more tags? (y/n)");
-           }
-        let tag_list_string = format!("[{}]",tag_list.join(", "));
-        return tag_list_string
+            println!("enter tag:");
+            let mut inputted_string = set_input().trim().to_string();
+            tag_list.push(inputted_string);
+            println!("the tag list is now {:?}", tag_list);
+            println!("do you want to enter more tags? (y/n)");
+        }
+        let tag_list_string = format!("[{}]", tag_list.join(", "));
+        return tag_list_string;
     }
-    
+
     println!("Enter tag input function? (y/n)");
     let tag_list = if set_input().to_lowercase().trim() == "y" {
         let array_result = set_tags();
         array_result
-    } else {"".to_string()};
-    
-    println!("{:?}",tag_list);
+    } else {
+        "".to_string()
+    };
+
+    println!("{:?}", tag_list);
 
     // Set date today
     let local: DateTime<Local> = Local::now();
     let date_today = format!("{}", local.format("%Y-%m-%d"));
     println!("date now is {}", date_today);
-    
+
     // function to request input from user
     fn set_input() -> String {
         let mut input_string = String::new();
         stdin().read_line(&mut input_string).unwrap();
         return input_string;
     }
-    
+
     // filter special characters from title
     fn filter_input(raw_string: &str) -> String {
-            let filtered_string = raw_string.to_lowercase()
+        let filtered_string = raw_string
+            .to_lowercase()
             .chars()
             .filter_map(|c| {
                 if c.is_alphanumeric() {
@@ -59,10 +60,11 @@ fn main() {
                 } else {
                     None
                 }
-            }).collect();
-            return filtered_string
+            })
+            .collect();
+        return filtered_string;
     }
-    
+
     // set title from input
     println!("write title");
     let title = set_input();
@@ -81,13 +83,14 @@ fn main() {
 
     let file = File::create(filename).expect("unable to create file");
     let mut file = BufWriter::new(file);
-    
+
     write!(file, "---\n").expect("failed to write top front matter dots");
     write!(file, "layout: post\n").expect("failed to write post layout to file");
     write!(file, "title: {}", title).expect("failed to write title to file");
     if !location.trim().is_empty() {
         println!("writing the location to file");
-        write!(file, "location: {}\n", location.trim()).expect("failed to write the location to file");
+        write!(file, "location: {}\n", location.trim())
+            .expect("failed to write the location to file");
     }
     if !tag_list.is_empty() {
         println!("writing tags to file");
@@ -95,5 +98,4 @@ fn main() {
     }
     write!(file, "---\n\n").expect("failed to write bottom front matter dots");
     println!("everything written to file");
-
 }
