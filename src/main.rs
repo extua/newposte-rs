8e45ae064e6ac4a5f506bf80499617c692ac220c
extua <pierre.marshall@gmail.com>
2023-11-10 18:57:20 +0800

    rearrange functions to fit the flow of the program

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,21 @@
 use chrono::prelude::*;
 use std::fs::File;
 use std::io::stdin;
-use std::io::stdout;
+//use std::io::stdout;
 use std::io::{BufWriter, Write};
 
 fn main() {
-    fn set_tags() -> String {
-        let mut tag_list: Vec<String> = Vec::new();
-        println!("enter tag:");
-        let mut inputted_string = set_input().trim().to_string();
-        tag_list.push(inputted_string);
-        println!("the tag list is {:?}", tag_list);
-        println!("do you want to enter more tags? (y/n)");
-        while set_input().to_lowercase().trim() == "y" {
-            println!("enter tag:");
-            let mut inputted_string = set_input().trim().to_string();
-            tag_list.push(inputted_string);
-            println!("the tag list is now {:?}", tag_list);
-            println!("do you want to enter more tags? (y/n)");
-        }
-        let tag_list_string = format!("[{}]", tag_list.join(", "));
-        return tag_list_string;
-    }
-
-    println!("Enter tag input function? (y/n)");
-    let tag_list = if set_input().to_lowercase().trim() == "y" {
-        let array_result = set_tags();
-        array_result
-    } else {
-        "".to_string()
-    };
-
-    println!("{:?}", tag_list);
-
-    // Set date today
-    let local: DateTime<Local> = Local::now();
-    let date_today = format!("{}", local.format("%Y-%m-%d"));
-    println!("date now is {}", date_today);
-
     // function to request input from user
     fn set_input() -> String {
         let mut input_string = String::new();
         stdin().read_line(&mut input_string).unwrap();
         return input_string;
     }
+
+    // Set date today
+    let local: DateTime<Local> = Local::now();
+    let date_today = format!("{}", local.format("%Y-%m-%d"));
+    println!("date now is {}", date_today);
 
     // filter special characters from title
     fn filter_input(raw_string: &str) -> String {
@@ -72,14 +44,43 @@ fn main() {
 
     println!("Lowercase title is {lowertitle}");
 
+    // Create file and write to it
+    let filename = format!("{}-{}.md", date_today, lowertitle);
+    println!("You set the filename as {}", filename);
+
+    // function to set and format tags
+    fn set_tags() -> String {
+        let mut tag_list: Vec<String> = Vec::new();
+        println!("enter tag:");
+        let inputted_string = set_input().trim().to_string();
+        tag_list.push(inputted_string);
+        println!("the tag list is {:?}", tag_list);
+        println!("do you want to enter more tags? (y/n)");
+        while set_input().to_lowercase().trim() == "y" {
+            println!("enter tag:");
+            let inputted_string = set_input().trim().to_string();
+            tag_list.push(inputted_string);
+            println!("the tag list is now {:?}", tag_list);
+            println!("do you want to enter more tags? (y/n)");
+        }
+        let tag_list_string = format!("[{}]", tag_list.join(", "));
+        return tag_list_string;
+    }
+
+    println!("Enter tag input function? (y/n)");
+    let tag_list = if set_input().to_lowercase().trim() == "y" {
+        let array_result = set_tags();
+        array_result
+    } else {
+        "".to_string()
+    };
+
+    println!("{:?}", tag_list);
+
     println!("write location");
     let location = set_input();
 
     println!("You set the location as {:?}", location);
-
-    // Create file and write to it
-    let filename = format!("{}-{}.md", date_today, lowertitle);
-    println!("You set the filename as {}", filename);
 
     let file = File::create(filename).expect("unable to create file");
     let mut file = BufWriter::new(file);
