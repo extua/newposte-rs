0803236b4bd8b2af0083a51cad3b71c74c6ba5e4
extua <pierre.marshall@gmail.com>
2023-11-12 14:30:40 +0800

    more work on string slicing

--- examples/listdir.rs
+++ examples/listdir.rs
@@ -1,9 +1,25 @@
 use std::fs;
 
 fn main() {
-  let paths = fs::read_dir("./").unwrap();
+    fn print_dir(path_end: &str) {
+        let mut entries_list: Vec<String> = Vec::new();
+        if let Ok(entries) = fs::read_dir(path_end) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    // Here, `entry` is a `DirEntry`.
+                    let filename = entry.file_name().into_string().unwrap();
+                    // slice last four characters from string
+                    let filetype = &filename[filename.len().saturating_sub(4)..filename.len()];
+                    if filetype == ".jpg" {
+                        println!("yapec here");
+                        entries_list.push(filename);
+                    };
 
-  for path in paths {
-    println!("{}", path.unwrap().path().display())
-  }
+                    println!("the tag list is now {:?}", entries_list);
+                }
+            }
+        }
+    }
+
+    print_dir("./")
 }
