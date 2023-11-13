457199a2f3fc448865e2b2ea36750ff5acc48333
extua <pierre.marshall@gmail.com>
2023-11-13 17:44:48 +0800

    change is_jpeg to its own function

--- examples/listdir.rs
+++ examples/listdir.rs
@@ -8,17 +8,32 @@ fn main() {
                 if let Ok(entry) = entry {
                     // Here, `entry` is a `DirEntry`.
                     let filename = entry.file_name().into_string().unwrap();
-                    // slice last four characters from string
-                    let filetype = &filename[filename.len().saturating_sub(4)..filename.len()];
-                    if filetype == ".jpg" {
-                        println!("yapec here");
-                        entries_list.push(filename);
-                    };
+                    // if filename is longer than 4 characters, slice last four characters from string
+                    fn is_jpeg(filename: &str) -> bool {
+                        if filename.len() > 4 {
+                            // saturating sub here will subtract up to the limit of the integer type (zero for unsigned integer)
+                            let filetype =
+                                &filename[filename.len().saturating_sub(4)..filename.len()];
+                            if filetype == ".jpg" {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }
 
-                    println!("the tag list is now {:?}", entries_list);
+                    if is_jpeg(&filename) {
+                        let formatted_filename =
+                            format!("% picture {}{} --alt {} %", &path_end, &filename, &filename);
+                        println!("yapec here, {}", formatted_filename);
+                        entries_list.push(filename);
+                    }
                 }
             }
         }
+        println!("entries are {:?}", entries_list);
     }
 
     print_dir("./")
