28d529fee3e1666041109aa8b32dc39080c4d9f3
extua <pierre.marshall@gmail.com>
2023-11-13 21:57:55 +0800

    Revert "restore listdir file, but this needs to be reverted with new git pull"
    This reverts commit 27c2591d8f2f704073683419a8f22a733b483645.

--- examples/listdir.rs
+++ examples/listdir.rs
@@ -9,26 +9,34 @@ fn main() {
                     // Here, `entry` is a `DirEntry`.
                     let filename = entry.file_name().into_string().unwrap();
                     // if filename is longer than 4 characters, slice last four characters from string
-                    fn is_jpeg(filename) {
-                        if filename.len > 4 {
-                            let filetype = &filename[filename.len().saturating_sub(4)..filename.len()];
-
+                    fn is_jpeg(filename: &str) -> bool {
+                        if filename.len() > 4 {
+                            // saturating sub here will subtract up to the limit of the integer type (zero for unsigned integer)
+                            let filetype =
+                                &filename[filename.len().saturating_sub(4)
+                                    ..filename.len()];
+                            if filetype == ".jpg" {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            return false;
                         }
                     }
-                    if filename.len() > 4 {
-                        // saturating sub here will subtract up to the limit of the integer type
-                        // (zero for unsigned integer)
-                      
-                        if filetype == ".jpg" {
-                            let formatted_filename = format!("% picture {}{} --alt {} %", &path_end,&filename,&filename);
-                            println!("yapec here, {}", formatted_filename);
-                            entries_list.push(filename);
-                        }
+
+                    if is_jpeg(&filename) {
+                        let formatted_filename = format!(
+                            "% picture {}{} --alt {} %",
+                            &path_end, &filename, &filename
+                        );
+                        println!("yapec here, {}", formatted_filename);
+                        entries_list.push(filename);
                     }
                 }
             }
-            println!("entries are {:?}", entries_list);
         }
+        println!("entries are {:?}", entries_list);
     }
 
     print_dir("./")
